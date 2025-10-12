151feeb3acbe48009d7bd5da106d4c8b47fa7008
RadhaKrishna-2020 <radhakrishna.pawankumar@gmail.com>
2025-10-12 13:30:31 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  }
+
+resource "aws_instance" "myec2" {
+   ami = "ami-052064a798f08f0d3"
+   instance_type = "t2.micro"
 }
